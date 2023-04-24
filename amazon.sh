7afca989c483165e420a737a1a1130479bb945f5
stonezone <stonezone@users.noreply.github.com>
2023-04-23 21:56:04 -1000

    Update amazon.sh

--- amazon.sh
+++ amazon.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
+# Log file for storing the logs
 LOG_FILE="amazon_setup.log"
 
+# Logging function to log messages with timestamps
 log() {
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a "$LOG_FILE"
+  printf "%s - %s\n" "$(date '+%Y-%m-%d %H:%M:%S')" "$1" | tee -a "$LOG_FILE"
 }
 
+# Function to check permissions to create the log file
 check_permissions() {
   touch "$LOG_FILE" 2> /dev/null
   if [ $? -ne 0 ]; then
@@ -14,6 +18,7 @@ check_permissions() {
   fi
 }
 
+# Function to open Google Chrome with the specified URL
 open_chrome() {
   url="$1"
 
@@ -32,56 +37,57 @@ open_chrome() {
   fi
 }
 
+# Function to set up an Amazon Seller Account
 set_up_seller_account() {
-  # Code for setting up an Amazon Seller Account
   log "Opening Amazon Seller Account creation page in Google Chrome..."
   account_creation_url="https://www.amazon.com/seller-account-creation"
   open_chrome "$account_creation_url"
 }
 
+# Function to set up an Amazon storefront
 set_up_storefront() {
-  # Code for setting up an Amazon storefront
   log "Opening Amazon Storefront setup page in Google Chrome..."
   storefront_setup_url="https://www.amazon.com/storefront-setup"
   open_chrome "$storefront_setup_url"
 }
 
+# Function to promote an Amazon storefront
 promote_storefront() {
-  # Code for promoting an Amazon storefront
   log "Opening Amazon Storefront promotion page in Google Chrome..."
   storefront_promotion_url="https://www.amazon.com/storefront-promotion"
   open_chrome "$storefront_promotion_url"
 }
 
+# Main script
 check_permissions
 log "Script started"
 
-echo "Welcome to the Amazon Seller Account and Storefront Setup and Promotion script!"
-echo "Which task would you like to perform?"
+printf "Welcome to the Amazon Seller Account and Storefront Setup and Promotion script!\n"
+printf "Which task would you like to perform?\n"
 
 TASK_SELLER_ACCOUNT=1
 TASK_STOREFRONT=2
 TASK_PROMOTION=3
 
-echo "$TASK_SELLER_ACCOUNT. Set up an Amazon Seller Account"
-echo "$TASK_STOREFRONT. Set up an Amazon storefront"
-echo "$TASK_PROMOTION. Promote an Amazon storefront"
+printf "%s. Set up an Amazon Seller Account\n" "$TASK_SELLER_ACCOUNT"
+printf "%s. Set up an Amazon storefront\n" "$TASK_STOREFRONT"
+printf "%s. Promote an Amazon storefront\n" "$TASK_PROMOTION"
 
 while true; do
-  read -p "Please enter the number of the task you want to perform: " task
+  read -rp "Please enter the number of the task you want to perform: " task
 
-  if [ "$task" == "$TASK_SELLER_ACCOUNT" ]; then
+  if [[ "$task" == "$TASK_SELLER_ACCOUNT" ]]; then
     set_up_seller_account
     break
-  elif [ "$task" == "$TASK_STOREFRONT" ]; then
+  elif [[ "$task" == "$TASK_STOREFRONT" ]]; then
     set_up_storefront
     break
-  elif [ "$task" == "$TASK_PROMOTION" ]; then
+  elif [[ "$task" == "$TASK_PROMOTION" ]]; then
     promote_storefront
     break
   else
     log "Invalid task number entered"
-    echo "Invalid task number. Please enter a valid task number."
+    printf "Invalid task number. Please enter a valid task number.\n"
   fi
 done
 
